90491a16ba818e60cf1891e1e38b8433513ade4c
James Gowans <jgowans@amazon.com>
2019-09-16 09:42:06 +0200

    Log swapped out data size
    We want this so we can know what percentage of the full guest size we've
    managed to swap out.
    Yucky hack reading /proc/pid/smaps and "grepping" for the Swap line.
    Really this should come from the sysinfo crate, and I'll look at
    extending it in the near future to know about segments.

--- mem_analyze/src/statistics.rs
+++ mem_analyze/src/statistics.rs
@@ -1,4 +1,5 @@
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
+use std::io::Read;
 use csv::Writer;
 use sysinfo::{System, SystemExt, ProcessExt, RefreshKind};
 
@@ -39,7 +40,7 @@ pub fn page_analytics(pid: i32, memory: &super::ProcessMemory) {
         zero_pages.to_string(),
         active_pages.to_string(),
         present_pages.to_string()];
-    append_process_stats(pid, &mut row);
+    append_process_stats(pid, memory, &mut row);
     let mut wtr = Writer::from_writer(
         OpenOptions::new().append(true).create(true)
             .open(format!("/tmp/wss/{}.csv", pid)).unwrap());
@@ -51,11 +52,44 @@ pub fn page_analytics(pid: i32, memory: &super::ProcessMemory) {
     }
 }
 
-fn append_process_stats(pid: i32, row: &mut Vec<String>) {
+fn append_process_stats(pid: i32, memory: &super::ProcessMemory, row: &mut Vec<String>) {
     // TODO: error handling. :P
     let mut system = System::new_with_specifics(RefreshKind::new());
     system.refresh_process(pid);
     let process = system.get_process(pid).unwrap();
     row.push(process.minflt().to_string());
     row.push(process.majflt().to_string());
+
+    let mut smaps = String::new();
+    File::open(format!("/proc/{}/smaps", pid)).unwrap().read_to_string(&mut smaps).unwrap();
+    let swap_usage: u64 = memory.segments.iter()
+        .map(|segment| swap_for_segment(&smaps, segment))
+        .fold(0,|a, b| a + b);
+    info!("Swap usage: {} kB", swap_usage >> 10);
+    row.push(swap_usage.to_string());
+}
+
+fn swap_for_segment(smaps: &str, segment: &super::Segment) -> u64 {
+    let target_line = format!("{:x}", segment.addr_start);
+    let mut lines = smaps.lines();
+    loop {
+        match lines.next() {
+            Some(line) => {
+                if line.starts_with(&target_line) {
+                    loop {
+                        match lines.next() {
+                            Some(line) => {
+                                // Swap:            3237444 kB
+                                if line.starts_with("Swap:") {
+                                    return u64::from_str_radix(line.split_ascii_whitespace().nth(1).unwrap(), 10).unwrap() << 10
+                                }
+                            },
+                            None => panic!("Got to end of smaps block withouht finding swap")
+                        }
+                    }
+                }
+            },
+            None => panic!("Got to end of smaps file without finding {}", target_line)
+        }
+    }
 }
